19e53bce24bfd4c2283d233a99984eb2ef371d61
Nidhi <nidhipatel439@gmail.com>
2022-04-01 13:56:11 -0400

    change in responsive

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var toggleButton = document.querySelector("menu-toggle");
-var menu = document.querySelector("#main-menu ul");
-
-toggleButton.addEventListener("click", function () {
-  menu.classList.toggle("show-menu");
-});
